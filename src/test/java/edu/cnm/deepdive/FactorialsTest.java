de14aa7a844af71a460f3b622ad81fd404564f45
Hana Dawson <stubbornunicorn95@gmail.com>
2021-03-09 10:12:05 -0700

    Method source returning List<Arguments>.

--- src/test/java/edu/cnm/deepdive/FactorialsTest.java
+++ src/test/java/edu/cnm/deepdive/FactorialsTest.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
@@ -32,13 +33,14 @@ class FactorialsTest {
     assertThrows(IllegalArgumentException.class, () -> Factorials.computeRecursive(-1));
   }
 
-  static Arguments[] provideTestCases() {
-    return new Arguments[] {
+  static List<Arguments> provideTestCases() {
+    return List.of(
         Arguments.of(0, 1L),
         Arguments.of(1, 1L),
         Arguments.of(5, 120L),
         Arguments.of(10, 3_628_800L),
         Arguments.of(13, 6_227_020_800L)
-    };
+    );
   }
+
 }
